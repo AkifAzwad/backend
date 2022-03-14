6dac25e6d44a5d58c9e19a385abf1143414a3a40
AkifAzwad <akifazwad6969@gmail.com>
2022-03-14 08:06:40 +0600

    added procfile

--- api/api/settings.py
+++ api/api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-25b3sv^+83b6jb3*tmlv++w-mf=%jxx_89_k)gjy3b2$3c0&l%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
