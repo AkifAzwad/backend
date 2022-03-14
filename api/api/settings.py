fa0906d5f9f9fc69602ee477fa15d3d344b7389b
AkifAzwad <akifazwad6969@gmail.com>
2022-03-14 09:24:30 +0600

    fixed bug

--- api/api/settings.py
+++ api/api/settings.py
@@ -127,6 +127,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS = BASE_DIR / 'staticfiles'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 import django_heroku
