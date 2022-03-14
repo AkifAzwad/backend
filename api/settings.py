724ba325b2a3004ed2eef5eec4e76684e44f9ecb
AkifAzwad <akifazwad6969@gmail.com>
2022-03-14 18:58:55 +0600

    edit

--- api/settings.py
+++ api/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT = str(BASE_DIR / 'static')
+STATIC_ROOT = str(BASE_DIR / 'staticfiles')
 
 STATICFILES_DIRS = [str(BASE_DIR / 'static')]
 
